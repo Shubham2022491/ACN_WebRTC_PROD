9c49cd7478fa117a1edc1e75f6efd7acf3aaab02
Shubham2022491 <119068073+Shubham2022491@users.noreply.github.com>
2025-04-13 23:43:12 +0530

    URLLLLL

--- fun.jsx
+++ fun.jsx
@@ -175,8 +175,11 @@ function App() {
     console.log("Initializing socket and WebRTC connections");
     
     // Initialize socket connection
-    socketRef.current = io("https://acn-webrtc-signaling-server-prod.onrender.com/");
-    
+    socketRef.current = io("wss://acn-webrtc-signaling-server-prod.onrender.com", {
+        transports: ['websocket'],
+        reconnectionAttempts: 5,
+        timeout: 5000,
+    });
     const createPeerConnection = (userSocketId) => {
       // Check if connection already exists
       if (peerConnectionsRef.current[userSocketId]) {
